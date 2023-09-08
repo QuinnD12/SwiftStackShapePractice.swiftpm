33de16c8f6d536205082390d21e0912c911b0838
Quinn Davis <qdavis6106@stu.d214.org>
2023-09-08 10:54:32 -0500

    Works Now

---
+++ StackView.swift
@@ -0,0 +1,15 @@
+//
+//  StackView.swift
+//  SwiftStackShapePractice
+//
+//  Created by Quinn B. Davis on 9/8/23.
+//
+
+import SwiftUI
+
+struct StackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
