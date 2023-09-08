67e1d3a12b16ae5ac80b32d5a5420472a5aeb5f8
Quinn Davis <qdavis6106@stu.d214.org>
2023-09-08 10:57:50 -0500

    Separate previews

--- StackView.swift
+++ StackView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct StackView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("123023085")
+        }
     }
 }
 
+struct StackViewPreview: PreviewProvider {
+    static var previews: some View {
+        StackView()
+    }
+}
