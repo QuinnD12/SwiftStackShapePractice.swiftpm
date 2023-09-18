5b5ed612688bebae7b59922e69e50058ac3468b8
Brock Wrede <bee15dub@gmail.com>
2023-09-18 10:30:24 -0500

    doozies

--- ShapeView.swift
+++ ShapeView.swift
@@ -19,6 +19,28 @@ struct ShapeView: View {
                    RoundedRectangle(cornerRadius: 10.0)
                        .foregroundColor(.red)
                        .frame(width: 200, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .foregroundColor(.black)
+                        .overlay(
+                            Rectangle()
+                                .frame(width: 55, height: 55, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(.blue))
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .frame(width: 300, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .overlay(
+                            Text("Click Me")
+                                .foregroundColor(.white)
+                                .font(.custom("American Typewriter", size: 35)))
+                    
+            Rectangle()
+                        .trim(from: 0.25, to: 0.5)
+                        .rotation(Angle(degrees: -45), anchor: .topLeading)
+                        .frame(width: 300, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                        .foregroundColor(.yellow)
+
         }
     }
 }
